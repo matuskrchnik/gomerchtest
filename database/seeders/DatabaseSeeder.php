bf597ebd532be6fe16fd8a0ccbe179ec3da269c9
Matus <matus.krchnik@gmail.com>
2022-01-07 13:32:51 +0100

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory(1)->create();
+        \App\Models\Test::factory(10)->create(); // pomocou facotry generujeme 10 stplcov mien do tabulky test
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api; 
+use App\Models\User;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('login', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]); 
+
+    $user = User::where('email', $request->email)->first();
+
+    // Overenie ci su udaje spravne
+    if ( !$user || !Hash::check($request->password, $user->password)) {
+        throw ValidationException::withMessages([
+            'email' => ['The provided credentials are incorrect.'],
+        ]);
+    }
+
+    return $user->createToken($request->email)->plainTextToken; // vytvorenie tokena pre uzivatela
+});
+
+Route::get('user', function (Request $request) {
+    return $request->user(); // vratenie dat uzivatela na zaklade tokena
+})->middleware('auth:sanctum');
+
+
+
+Route::get('test', [Api\TestController::class, 'index']); 
